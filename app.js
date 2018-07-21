714bc596b892f4eccc4ecdd0a300daf4c9448a57
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-07-21 16:02:41 +1200

    added init function

--- app.js
+++ app.js
@@ -103,7 +103,11 @@ var UIController = (function() {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
     
     return {
@@ -147,7 +151,16 @@ var UIController = (function() {
         },
 
         displayBudget: function(obj) {
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
             
+
+            if (obj.percentage > 0){
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
         },
 
         getDOMstrings: function() {
@@ -178,7 +191,7 @@ var updateBudget = function() {
     //return budget
     var budget = budgetCtrl.getBudget();
     //display budget on UI
-    console.log(budget);
+    UICtrl.displayBudget(budget);
 };
     
 
@@ -205,6 +218,12 @@ var ctrlAddItem = function () {
 return {
     init: function(){
         console.log('Application has started.');
+        UICtrl.displayBudget({
+            budget: 0, 
+            totalInc: 0,
+            totalExp: 0,
+            percentage: -1
+        });
         setupEventListeners();
     }
 };
